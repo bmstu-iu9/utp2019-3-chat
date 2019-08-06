9880adf15b2632535f6fefe2d6f7e62359d40a1d
VarenytsiaMykhailo <l2fonat@bk.ru>
2019-08-06 20:32:43 +0300

    работа с функциями в db.js
    - мелкие изменения
    - в db.js добавил функцию setClientLoggedIn
    - в db.js добавил функцию
    checkClientLoggedIn

--- chat/db/db.js
+++ chat/db/db.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-const pathDbUsers = path.join(__dirname, 'users.txt');
-const pathDbChat = path.join(__dirname, 'chat.txt');
+const pathDbUsers = path.join(__dirname, 'users.txt'); //путь к файлу users.txt
+const pathDbChat = path.join(__dirname, 'chat.txt'); //путь к файлу chat.txt
+const clientsLoggedIn = []; //массив клиентов, в котором хранятся находящиеся в онлайне клиенты (объекты со значениями логин + ключ)
 
-const searchLogin = function (login, log = false) {
+const searchLogin = function (login, log = false) { //поиск переданного логина в файле users.txt
     let users = fs.readFileSync(pathDbUsers, 'utf-8').split('\n');
     for (let i = 0; i < users.length - 1; i++) {
         let separated = users[i].split(' ');
         if (login === separated[0]) {
-            if (log) console.log(`login: ${separated[0]} logged in`);
+            if (log) console.log(`login: ${separated[0]} logged in`);//отладочная печать
             return true;
         }
     }
@@ -16,25 +17,23 @@ const searchLogin = function (login, log = false) {
     return false;
 };
 
-const searchLoginAndPassword = function (login, password, log = false) {
+const searchLoginAndPassword = function (login, password, log = false) {//поиск переданного логина+пароля в users.txt
     let users = fs.readFileSync(pathDbUsers, 'utf-8').split('\n');
     for (let i = 0; i < users.length - 1; i++) {
         let separated = users[i].split(' ');
         if (login === separated[0] && password === separated[1]) {
-            if (log) console.log(`login: ${separated[0]} logged in`);
+            if (log) console.log(`login: ${separated[0]} logged in`);//отладочная печать
             return true;
         }
     }
-    if (log) console.log(`login: ${login} attempt to log in`);
+    if (log) console.log(`login: ${login} attempt to log in`);//отладочная печать
     return false;
 };
 
-const setNewUser = function (login, password, log = false){
-    if (!searchLogin(login)) {
+const setNewUser = function (login, password, log = false){//запись пользователя (логин + пароль) в users.txt (юзается при регистрации)
+    if (searchLogin(login) === false) {
         fs.appendFile(pathDbUsers, login + ' ' + password + '\n', err => {
-            if (log) {
-                console.log('add new user with login: ' + login);
-            }
+            if (log) console.log('add new user with login: ' + login); //отладочная печать
         });
         return true;
     }else {
@@ -50,9 +49,31 @@ const getMessage = function (obj) {
     return fs.readFileSync(pathDbChat, 'utf-8').split('\n');
 };
 
+const setClientLoggedIn = function (login, key, log = false){ //занести клиента в массив clientsLoggedIn (массив пользователей, которые в онлайне)
+    clientsLoggedIn.push(
+        {
+            login : login,
+            key : key
+        }
+        );
+        if (log) console.log(clientsLoggedIn); //отладочная печать
+};
+
+const checkClientLoggedIn = function (login, key){ //проверить, находится ли пользователь в онлайне (в массиве clientsLoggedIn)
+        for (let client of clientsLoggedIn){
+            if (client.login === login && client.key === key){
+                return true;
+            }
+        }
+        return false;
+};
+
 module.exports = {
     searchLoginAndPassword : searchLoginAndPassword,
     setNewUser : setNewUser,
     getMessage: getMessage,
-    setMessage : setMessage
+    setMessage : setMessage,
+    setClientLoggedIn : setClientLoggedIn,
+    checkClientLoggedIn : checkClientLoggedIn
+
 };
