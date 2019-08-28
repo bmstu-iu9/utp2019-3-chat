f21f6baa166a6f1cc18fe5c0a7ed29bd678664ad
avk1221 <kicheeva_av@mail.ru>
2019-08-28 15:28:11 +0300

    Cоздала объект валидатора validatorSignIn

--- chat/public/login.js
+++ chat/public/login.js
@@ -1,3 +1,19 @@
 const URL = 'http://localhost:3000/';
 const nameFormSignIn = 'form-signin';
 const nameFormSignUp = 'form-signup';
+
+const validatorSignIn = new Validator({
+    id: nameFormSignIn,
+    method: {
+        'login': [
+            ['notEmpty']
+        ],
+        'password': [
+            ['notEmpty']
+        ],
+        'error_mess': {
+            'login': 'Enter your login',
+            'password': 'Enter your password'
+        }
+    }
+});
