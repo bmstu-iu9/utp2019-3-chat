741c1cfe5ba5b41e4e18a3cd4424ff717a6d644f
VarenytsiaMykhailo <l2fonat@bk.ru>
2019-09-08 13:45:24 +0300

    работа с db.js . Реализованы новые методы
    * косметические работы
    *реализован метод searchUser (замена старым методам searchLoginAndPassword и searchLogin. Объединил их в один метод).
    *реализация метода getMessage.
    *добавлена новая реализация метода deleteClientLoggedIn. Старый переименован в deleteClientLoggedIn2.

--- chat/db/db.js
+++ chat/db/db.js
@@ -5,9 +5,29 @@ const pathDbChat = path.join(__dirname, 'chat.txt'); //путь к файлу ch
 
 class DB {
     constructor(){
-        this.clientLoggedIn = []; //массив клиентов, в котором хранятся находящиеся в онлайне клиенты (объекты со значениями логин + ключ)
+        this.clientsLoggedIn = []; //массив клиентов, в котором хранятся находящиеся в онлайне клиенты (объекты со значениями логин + ключ)
     }
 
+    searchUser(login, password=null, log=false) {
+        let users = fs.readFileSync(pathDbUsers, 'utf-8').split('\n');
+        for (let i = 0; i < users.length - 1; i++) {
+            let separated = users[i].split(' ');
+            if (password) {
+                if (login === separated[0] && password === separated[1]) {
+                    if (log) console.log(`name: ${separated[0]} logged in`);
+                    return true;
+                }
+            } else {
+                if (login === separated[0]) {
+                    if (log) console.log(`name: ${separated[0]} logged in`);
+                    return true;
+                }
+            }
+        }
+        if (log) console.log(`name: ${login} attempt to log in`);
+        return false;
+    };
+    //старая версия реализации searchUser
     searchLogin(login, log = false) { //поиск переданного логина в файле users.txt
         let users = fs.readFileSync(pathDbUsers, 'utf-8').split('\n');
         for (let i = 0; i < users.length - 1; i++) {
@@ -20,8 +40,7 @@ class DB {
         if (log) console.log(`login: ${login} attempt to log in`);
         return false;
     };
-
-
+    //старая версия реализации searchUser
     searchLoginAndPassword(login, password, log = false) {//поиск переданного логина+пароля в users.txt
         let users = fs.readFileSync(pathDbUsers, 'utf-8').split('\n');
         for (let i = 0; i < users.length - 1; i++) {
@@ -34,14 +53,15 @@ class DB {
         if (log) console.log(`login: ${login} attempt to log in`);//отладочная печать
         return false;
     };
-
-    setNewUser(login, password, log = false) {//запись пользователя (логин + пароль) в users.txt (юзается при регистрации)
-        if (this.searchLogin(login) === false) {
+    //запись пользователя (логин + пароль) в users.txt (юзается при регистрации)
+    setNewUser(login, password, log=false) {
+        if (!this.searchUser(login)) {
             fs.appendFile(pathDbUsers, login + ' ' + password + '\n', err => {
-                if (log) console.log('add new user with login: ' + login); //отладочная печать
+                if (log) console.log('add new user: ' + login);
             });
             return true;
         } else {
+            if (log) console.log(`name: ${login} attempt to sign up`);
             return false;
         }
     };
@@ -50,11 +70,17 @@ class DB {
         fs.appendFile(pathDbChat, obj + '\n', err => '');
     };
 
-    getMessage(obj) {
-        return fs.readFileSync(pathDbChat, 'utf-8').split('\n');
+    getMessage(room) {
+        const chat = fs.readFileSync(pathDbChat, 'utf-8').split('\n');
+        const chatMess = [];
+        for (let i = 0; i < chat.length - 1; i++) {
+            if (JSON.parse(chat[i]).room === room) chatMess.push(chat[i]);
+        }
+        console.log(chatMess);
+        return chatMess;
     };
-
-    setClientLoggedIn(login, key, log = false) { //занести клиента в массив clientsLoggedIn (массив пользователей, которые в онлайне)
+    //занести клиента в массив clientsLoggedIn (массив пользователей, которые в онлайне)
+    setClientLoggedIn(login, key, log = false) {
         for (let item of this.clientsLoggedIn) {
             if (item.login === login && item.key === key) {
                 if (log)
@@ -62,15 +88,14 @@ class DB {
                 return;
             }
         }
-        this.clientsLoggedIn.push( {
-                login: login,
-                key: key
-            }
-        );
+        this.clientsLoggedIn.push({
+            login: login,
+            key: key
+        });
         if (log) console.log(this.clientsLoggedIn); //отладочная печать
     };
-
-    checkClientLoggedIn(login, key) { //проверить, находится ли пользователь в онлайне (в массиве clientsLoggedIn)
+    //проверить, находится ли пользователь в онлайне (в массиве clientsLoggedIn)
+    checkClientLoggedIn(login, key) {
         for (let client of this.clientsLoggedIn) {
             if (client.login === login && client.key === key) {
                 return true;
@@ -78,8 +103,13 @@ class DB {
         }
         return false;
     };
-
-    deleteClientLoggedIn(login, key, log = false) {
+    //новая реализация метода. Старая - ниже
+    deleteClientLoggedIn(login, key, log=false) {
+        this.clientsLoggedIn.splice(this.clientsLoggedIn.findIndex(client => client.login === login && client.key === key), 1);
+        if (log) console.log('deleted: ' + login + ' key: ' + key);
+    };
+    //старая версия реализации метода
+    deleteClientLoggedIn2(login, key, log = false) {
         let index;
         for (let i = 0; i < this.clientsLoggedIn.length; i++) {
             if ((login === this.clientsLoggedIn[i].login) && (key === this.clientsLoggedIn[i].key)) {
